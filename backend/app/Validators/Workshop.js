0e51119292a7f31e64a19ec45930426434cdb7cb
Heitor A Neto <heitorh3@gmail.com>
2019-09-25 09:32:46 -0300

    feature/CUstom Validator

---
+++ backend/app/Validators/Workshop.js
@@ -0,0 +1,13 @@
+const { rule } = use('Validator');
+class Workshop {
+  get rules() {
+    return {
+      title: [rule('required')],
+      description: [rule('required')],
+      section: [rule('required'), rule('in', [1, 2, 3])],
+      user_id: [rule('required'), rule('exists', ['users', 'id'])],
+    };
+  }
+}
+
+module.exports = Workshop;
--- backend/start/app.js
+++ backend/start/app.js
@@ -1,5 +1,4 @@
-
-
+const path = require('path');
 /*
 |--------------------------------------------------------------------------
 | Providers
@@ -11,14 +10,15 @@
 |
 */
 const providers = [
-  "@adonisjs/framework/providers/AppProvider",
-  "@adonisjs/auth/providers/AuthProvider",
-  "@adonisjs/bodyparser/providers/BodyParserProvider",
-  "@adonisjs/cors/providers/CorsProvider",
-  "@adonisjs/lucid/providers/LucidProvider",
-  "@adonisjs/mail/providers/MailProvider",
-  "@adonisjs/framework/providers/ViewProvider",
-  "@adonisjs/validator/providers/ValidatorProvider"
+  '@adonisjs/framework/providers/AppProvider',
+  '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/bodyparser/providers/BodyParserProvider',
+  '@adonisjs/cors/providers/CorsProvider',
+  '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/mail/providers/MailProvider',
+  '@adonisjs/framework/providers/ViewProvider',
+  '@adonisjs/validator/providers/ValidatorProvider',
+  path.join(__dirname, '..', 'providers', 'CustomValidatorProvider'),
 ];
 
 /*
@@ -31,8 +31,8 @@ const providers = [
 |
 */
 const aceProviders = [
-  "@adonisjs/lucid/providers/MigrationsProvider",
-  "@adonisjs/vow/providers/VowProvider"
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  '@adonisjs/vow/providers/VowProvider',
 ];
 
 /*
